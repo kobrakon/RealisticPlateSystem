aebdb65bf4a6a2ea4d9673b261376248b6d43ce3
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2023-02-23 12:31:22 -0500

    upload

---
+++ types/models/spt/config/ITraderConfig.d.ts
@@ -0,0 +1,33 @@
+import { MinMax } from "../../../models/common/MinMax";
+import { IBaseConfig } from "./IBaseConfig";
+export interface ITraderConfig extends IBaseConfig {
+    kind: "aki-trader";
+    updateTime: UpdateTime[];
+    updateTimeDefault: number;
+    traderPriceMultipler: number;
+    persistPurchaseDataInProfile: boolean;
+    fence: FenceConfig;
+}
+export interface UpdateTime {
+    traderId: string;
+    seconds: number;
+}
+export interface FenceConfig {
+    discountOptions: DiscountOptions;
+    partialRefreshTimeSeconds: number;
+    partialRefreshChangePercent: number;
+    assortSize: number;
+    maxPresetsPercent: number;
+    itemPriceMult: number;
+    presetPriceMult: number;
+    armorMaxDurabilityPercentMinMax: MinMax;
+    presetMaxDurabilityPercentMinMax: MinMax;
+    itemTypeLimits: Record<string, number>;
+    regenerateAssortsOnRefresh: boolean;
+    blacklist: string[];
+}
+export interface DiscountOptions {
+    assortSize: number;
+    itemPriceMult: number;
+    presetPriceMult: number;
+}
